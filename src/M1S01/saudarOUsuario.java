1bf8aeee37a190b9e992a06dec9628d7a1af9229
Mah <marinalarissacarpesrohrig@gmail.com>
2022-07-08 19:32:46 -0300

    primeiro exercicio

---
+++ src/M1S01/saudarOUsuario.java
@@ -0,0 +1,9 @@
+package M1S01;
+// Exercício 1 da semana 1 do FuturoDEV
+// Autor: Marina Larissa Carpes Röhrig
+
+public class saudarOUsuario {
+    public static void main(String[] args) {
+        System.out.print("Olá, este programa foi escrito pela aluna Marina, no dia 08/07/22 em JAVA na IDE intellij. ");
+    }
+}
